6c3b048ae424c47f3743061f3354e52abb68b871
Helmi Al Farel <45677842+HelmiAlf@users.noreply.github.com>
2023-09-04 09:19:23 +0700

    Add Error exception for account cannot be found

--- corsproxy/views.py
+++ corsproxy/views.py
@@ -18,18 +18,31 @@ def index(request):
 	# print(request.GET)
 	if 'username' in request.GET.keys():
 
-		username, img_src, bio = get_user_data(request.GET["username"])
+		try:
+			username, img_src, bio = get_user_data(request.GET["username"])
 
-		response = JsonResponse({
-			"name": username,
-			"img_src": img_src,
-			"bio": bio
-			})
-		response["Access-Control-Allow-Origin"] = "*"
-		response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
-		response["Access-Control-Max-Age"] = "1000"
-		response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
-		return response
+			response = JsonResponse({
+				"name": username,
+				"img_src": 'img_src',
+				"bio": bio
+				})
+			response["Access-Control-Allow-Origin"] = "*"
+			response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+			response["Access-Control-Max-Age"] = "1000"
+			response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+			return response
+		except Exception as e:
+			response = JsonResponse({
+				"name": 'failed locating account',
+				"img_src": 'https://cdn-icons-png.flaticon.com/512/4436/4436559.png',
+				"bio": str(e)
+				})
+			response["Access-Control-Allow-Origin"] = "*"
+			response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+			response["Access-Control-Max-Age"] = "1000"
+			response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+			return response
+
 
 	else:
 		return HttpResponse("use /?username=(snackvideo_id)")
